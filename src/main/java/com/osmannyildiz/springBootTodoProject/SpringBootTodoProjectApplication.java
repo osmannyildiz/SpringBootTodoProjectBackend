225c77acea028567f73dbfbd8c594d8739df8e0f
Osman Nuri Yıldız <iamosmannyildiz@gmail.com>
2022-05-14 23:15:22 +0300

    Create Spring Boot project

---
+++ src/main/java/com/osmannyildiz/springBootTodoProject/SpringBootTodoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.osmannyildiz.springBootTodoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTodoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTodoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osmannyildiz/springBootTodoProject/SpringBootTodoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osmannyildiz.springBootTodoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTodoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
